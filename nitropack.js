923387de29370ec1b41ab1b6ceacb4da86151efe
WP-Akash <72037575+WP-Akash@users.noreply.github.com>
2022-02-08 19:26:11 +0600

    Nitropack Code
    Simple use this code. Hide Nitropack footer Watermark.

---
+++ nitropack.js
@@ -0,0 +1,2 @@
+<script> document.addEventListener("DOMContentLoaded", function(){ let divc = document.querySelectorAll('div[style]'); for (let i = 0, len = divc.length; i < len; i++) { let actdisplay = window.getComputedStyle(divc[i], null).display; let actclear = window.getComputedStyle(divc[i], null).clear; if(actdisplay == 'block' && actclear == 'both') { divc[i].remove(); } } });
+</script>
